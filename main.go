cd664b6a6696bb4730744ce6aa4319d55f374cc8
Jason Chen <jason.chen@clearmatics.com>
2022-12-28 12:06:44 +0000

    feature, add graceful shutdown for oracle service.

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package autonity_oralce
 import (
 	"autonity-oralce/types"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	// todo: register a signal handler to exit the server for a clean shutdown.
-
+	// todo: resolve configs from system environment variables.
 	var config types.OracleServiceConfig
 	// set default configs
 	config.HttpPort = 8080
@@ -20,6 +25,7 @@ func main() {
 	// create oracle service.
 	oracle := NewOracleService(&config)
 	go oracle.Start()
+	defer oracle.Stop()
 
 	// create http endpoint for data service.
 	router := gin.Default()
@@ -81,5 +87,36 @@ func main() {
 		}
 	})
 
-	router.Run(fmt.Sprintf(":%d", config.HttpPort))
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", config.HttpPort),
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
