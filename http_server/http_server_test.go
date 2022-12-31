91de4610a7f3d88d11983a4bd7ac8b1577c74cfc
Jason Chen <jason.chen@clearmatics.com>
2022-12-31 05:47:30 +0000

    test, add unit testcases for http server.

--- http_server/http_server_test.go
+++ http_server/http_server_test.go
@@ -1,12 +1,101 @@
 package http_server
 
 import (
+	"autonity-oralce/config"
+	"autonity-oralce/oracle_server"
+	"autonity-oralce/types"
+	"bytes"
+	"encoding/json"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/require"
+	"net/http"
 	"testing"
 )
 
 // todo: add tests for http api handlers.
 func TestHttpServerAPIHandlers(t *testing.T) {
-	t.Run("test updateSymbols handler", func(t *testing.T) {
+	conf := config.MakeConfig()
+	t.Run("test get version", func(t *testing.T) {
+		reqMsg := &types.JsonRpcMessage{
+			ID:     json.RawMessage{0},
+			Method: "get_version",
+			Params: nil,
+			Result: nil,
+			Error:  "",
+		}
 
+		oracle := oracle_server.NewOracleServer(conf.Symbols)
+		hs := NewHttpServer(oracle, conf.HttpPort)
+
+		code, rspMsg := hs.getVersion(reqMsg)
+		require.Equal(t, http.StatusOK, code)
+		require.Equal(t, reqMsg.ID, rspMsg.ID)
+		type Version struct {
+			Version string
+		}
+		dec := json.NewDecoder(bytes.NewReader(rspMsg.Result))
+		var v Version
+		err := dec.Decode(&v)
+		require.NoError(t, err)
+
+		require.Equal(t, hs.oracle.Version(), v.Version)
+	})
+
+	t.Run("test get prices", func(t *testing.T) {
+		reqMsg := &types.JsonRpcMessage{
+			ID:     json.RawMessage{0},
+			Method: "get_prices",
+			Params: nil,
+			Result: nil,
+			Error:  "",
+		}
+		oracle := oracle_server.NewOracleServer(conf.Symbols)
+		for _, s := range conf.Symbols {
+			price := types.Price{
+				Timestamp: 0,
+				Symbol:    s,
+				Price:     decimal.RequireFromString("12.33"),
+			}
+			oracle.UpdatePrice(price)
+		}
+		hs := NewHttpServer(oracle, conf.HttpPort)
+		code, rspMsg := hs.getPrices(reqMsg)
+		require.Equal(t, http.StatusOK, code)
+		require.Equal(t, reqMsg.ID, rspMsg.ID)
+		type PriceAndSymbol struct {
+			Prices  types.PriceBySymbol
+			Symbols []string
+		}
+
+		dec := json.NewDecoder(bytes.NewReader(rspMsg.Result))
+		var prices PriceAndSymbol
+		err := dec.Decode(&prices)
+		require.NoError(t, err)
+		for _, s := range conf.Symbols {
+			require.Equal(t, s, prices.Prices[s].Symbol)
+			require.Equal(t, true, prices.Prices[s].Price.Equals(decimal.RequireFromString("12.33")))
+		}
+		require.Equal(t, conf.Symbols, prices.Symbols)
+	})
+
+	t.Run("update symbols", func(t *testing.T) {
+		newSymbols := []string{"NTNETH", "NTNBTC", "NTNBNB"}
+		encSymbols, err := json.Marshal(newSymbols)
+		require.NoError(t, err)
+
+		reqMsg := &types.JsonRpcMessage{
+			ID:     json.RawMessage{0},
+			Method: "update_symbols",
+			Params: encSymbols,
+			Result: nil,
+			Error:  "",
+		}
+		oracle := oracle_server.NewOracleServer(conf.Symbols)
+		hs := NewHttpServer(oracle, conf.HttpPort)
+		code, rspMsg := hs.updateSymbols(reqMsg)
+		require.Equal(t, http.StatusOK, code)
+		require.Equal(t, reqMsg.ID, rspMsg.ID)
+		require.Equal(t, reqMsg.Params, rspMsg.Result)
+		require.Equal(t, newSymbols, oracle.Symbols())
 	})
 }
