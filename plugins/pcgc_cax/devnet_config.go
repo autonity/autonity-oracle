d97710cd0abf4b7e0f44b751e46f8972d37f623c
Jason Chen <jason.chen@clearmatics.com>
2023-12-01 09:53:04 +0000

    doc, update comments.

--- plugins/pcgc_cax/devnet_config.go
+++ plugins/pcgc_cax/devnet_config.go
@@ -3,7 +3,7 @@
 package main
 
 func init() {
-	// default setting for dev-network, to be enabled by build flag: DVE_NET
+	// default setting for dev-network, to be enabled by build flag: dev
 	routers = "orderbooks"
 	defaultEndpoint = "cax.devnet.clearmatics.network"
 }
