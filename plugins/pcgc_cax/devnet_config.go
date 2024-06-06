4b52e84ba02704b4bfd35e491048b319b943cf1b
Jason Chen <jason.chen@clearmatics.com>
2024-06-06 14:53:48 +0100

    config, update the url for pcgc_cax plugin for Autonity dev network.

--- plugins/pcgc_cax/devnet_config.go
+++ plugins/pcgc_cax/devnet_config.go
@@ -4,6 +4,5 @@ package main
 
 func init() {
 	// default setting for dev-network, to be enabled by build flag: dev
-	routers = "orderbooks"
 	defaultEndpoint = "cax.devnet.clearmatics.network"
 }
--- plugins/pcgc_cax/pcgc_cax_test.go
+++ plugins/pcgc_cax/pcgc_cax_test.go
@@ -6,10 +6,8 @@ import (
 )
 
 func TestNewCAXClient(t *testing.T) {
-	t.Skipf("skipped duo to the remote service endpoint is not available")
 	// set the CAX of dev-net for testing by default since piccadilly CAX is not ready.
 	defaultConfig.Endpoint = "cax.devnet.clearmatics.network"
-	routers = "orderbooks"
 	client := NewCAXClient(&defaultConfig)
 	prices, err := client.FetchPrice([]string{"ATN-USD", "NTN-USD", "NTN-ATN"})
 	require.NoError(t, err)
