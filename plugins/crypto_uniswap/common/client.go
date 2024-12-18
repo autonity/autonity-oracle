3e6e351b2c0b856b072f4e373e613c4fbc37caa4
Jason Chen <jason.chen@clearmatics.com>
2024-12-18 06:18:52 +0000

    lint, fix lint error.

--- plugins/crypto_uniswap/common/client.go
+++ plugins/crypto_uniswap/common/client.go
@@ -21,18 +21,6 @@ var (
 	NTNTokenAddress  = types.AutonityContractAddress // Autonity protocol contract is the NTN token contract.
 )
 
-var defaultConfig = types.PluginConfig{
-	Name:               "crypto_uniswap",
-	Scheme:             "wss",                                        // both http/s ws/s works for this plugin
-	Endpoint:           "rpc-internal-1.piccadilly.autonity.org/ws",  // default websocket endpoint for piccadilly network.
-	Timeout:            10,                                           // 10s
-	DataUpdateInterval: common.DefaultAMMDataUpdateInterval,          // 1s, shorten the default data point refresh interval for AMM market data, as they can move very fast.
-	NTNTokenAddress:    NTNTokenAddress.Hex(),                        // Same as 0xBd770416a3345F91E4B34576cb804a576fa48EB1, Autonity contract address.
-	ATNTokenAddress:    "0xcE17e51cE4F0417A1aB31a3c5d6831ff3BbFa1d2", // Wrapped ATN ERC20 contract address on the target blockchain.
-	USDCTokenAddress:   "0xB855D5e83363A4494e09f0Bb3152A70d3f161940", // USDCx ERC20 contract address on the target blockchain.
-	SwapAddress:        "0x218F76e357594C82Cc29A88B90dd67b180827c88", // UniSwap factory contract address on the target blockchain.
-}
-
 type WrappedPair struct {
 	pairContract *pair.Pair
 	token0       ecommon.Address
