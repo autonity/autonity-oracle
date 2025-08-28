0d1f96ead7588029a4fb45efe18b97e9e0e9cea5
Jason Chen <jason.chen@clearmatics.com>
2025-08-28 16:15:49 +0100

    logging, print more info in the greeting head of the log.

--- main.go
+++ main.go
@@ -19,9 +19,10 @@ import (
 
 func main() { //nolint
 	conf := config.MakeConfig()
-	log.Printf("\n\n\n \tRunning autonity oracle server %s\n\twith plugin directory: %s\n "+
-		"\tby connecting to L1 node: %s\n \ton oracle contract address: %s \n\n\n",
-		config.VersionString(config.Version), conf.PluginDIR, conf.AutonityWSUrl, types.OracleContractAddress)
+	log.Printf("\n\n\n \tRunning autonity oracle server %s\n\twith account: %s\n\twith plugin directory: %s\n "+
+		"\twith profile data direcotry: %s\n "+"\tby connecting to L1 node: %s\n \ton oracle contract address: %s \n\n\n",
+		config.VersionString(config.Version), conf.Key.Address.String(), conf.PluginDIR, conf.ProfileDir,
+		conf.AutonityWSUrl, types.OracleContractAddress)
 
 	// start prometheus metrics exposer if it is enabled.
 	if conf.MetricConfigs.EnablePrometheusExp {
